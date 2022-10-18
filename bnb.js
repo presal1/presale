dc942d20f44e64f68bb7a89dc186576c0c398059
presal1 <115598820+presal1@users.noreply.github.com>
2022-10-18 01:28:10 +0100

    Update bnb.js

--- bnb.js
+++ bnb.js
@@ -1,145 +1,269 @@
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import WalletLink from "walletlink";
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
+"use strict";
+
+/**
+ * Example JavaScript code that interacts with the page and Web3 wallets
+ */
+
+ // Unpkg imports
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+const EvmChains = window.EvmChains;
+const Fortmatic = window.Fortmatic;
+
+// Web3modal instance
+let web3Modal
+
+// Chosen wallet provider given by the dialog window
+let provider;
 
 
-var input = document.getElementById("in").value;
-var out = document.getElementById("out");
-
-function calculateAmount(value) {
-  var l;
-  var y = value;
-  l = y * 50000;
-  document.getElementById("out").value = l;
-}
-
-function calculatAmount(value) {
-  var a;
-  var o = value;
-  a = o / 50000;
-  document.getElementById("in").value = a;
-}
-
-​
-// Create a connector
-const connector = new WalletConnect({
-  bridge: "https://bridge.walletconnect.org", // Required
-  qrcodeModal: QRCodeModal,
-});
-​
-// Check if connection is already established
-if (!connector.connected) {
-  // create new session
-  connector.createSession();
-}
-​
-// Subscribe to connection events
-connector.on("connect", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-​
-  // Get provided accounts and chainId
-  const { accounts, chainId } = payload.params[0];
-});
-​
-connector.on("session_update", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-​
-  // Get updated accounts and chainId
-  const { accounts, chainId } = payload.params[0];
-});
-​
-connector.on("disconnect", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-​
-  // Delete connector
-});
+// Address of the selected account
+let selectedAccount;
 
 
-const web3Modal = new Web3Modal({
-  network: "rinkeby",
-  theme: "dark",
-  cacheProvider: true,
-  providerOptions 
-});
+/**
+ * Setup the orchestra
+ */
 
+ var input = document.getElementById("in").value;
+ var out = document.getElementById("out");
+ 
+ function calculateAmount(value) {
+   var l;
+   var y = value;
+   l = y * 50000;
+   document.getElementById("out").value = l;
+ }
+ 
+ function calculatAmount(value) {
+   var a;
+   var o = value;
+   a = o / 50000;
+   document.getElementById("in").value = a;
+ }
 
-loginButton = document.getElementById("loginButton")
+function init() {
 
-const userWallet = document.getElementById("userWallet");
+  console.log("Initializing example");
+  console.log("WalletConnectProvider is", WalletConnectProvider);
+  console.log("Fortmatic is", Fortmatic);
 
-loginButton.addEventListener("click", connectwallet);
+  
 
-async function connectwallet() { 
-	  var provider = await web3Modal.connect();
-      var web3 = new Web3(provider); 
-      await window.ethereum.send('eth_requestAccounts'); 
-      var accounts = await web3.eth.getAccounts(); 
-      account = accounts[0]; 
-      document.getElementById('userWallet').textContent = account; 
-      contract = new web3.eth.Contract(ABI, ADDRESS);
-	//   userWallet.innerText = ADDRESS;
-}
-const providerOptions = {
-	binancechainwallet: {
-		package: true
-	  },
-	walletconnect: {
-		package: WalletConnectProvider,
-		options: {
-		  infuraId: "765d4237ce7e4d999f706854d5b66fdc"
-		}
-	  },
-	  walletlink: {
-		package: WalletLink, 
-		options: {
-		  appName: "Net2Dev NFT Minter", 
-		  infuraId: "765d4237ce7e4d999f706854d5b66fdc", 
-		  rpc: "", 
-		  chainId: 1, 
-		  appLogoUrl: null, 
-		  darkMode: true 
-		}
-	  },
-};
-// async function mint() {
-//         var _mintAmount = Number(document.querySelector("[name=amount]").value); 
-//         var mintRate = Number(await contract.methods.cost().call()); 
-//         var totalAmount = mintRate * _mintAmount; 
-//       contract.methods.mint(account, _mintAmount).send({ from: account, value: String(totalAmount) }); 
-// } 
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+      
+        infuraId: "bdd7d542dfa44290912acdc96d3cae36",
+      }
+    },
 
-const sendButton = document.getElementById("send-button");
+    fortmatic: {
+      package: Fortmatic,
+      options: {
+        
+        key: "pk_test_391E26A3B43A3350"
+      }
+    }
+  };
 
-
-sendButton.addEventListener("click", () => {
-const tx = {
-	from: account,
-	to: "0xe8eb7788A5bDd0Dcd2086aC6045d66656ef67203",
-	value: Number(document.getElementById("in").value * 1e18).toString( 16),
-}
-WalletConnect.sendTransaction(tx)
-.then((result) => {
-	Swal.fire({
-        title: "success",
-        text: "Goood",
-        icon: "success",
-        confirmButtonText: "Ok",
-      })
-})
-.catch((error) => {
-	Swal.fire({
-		title: "Error!",
-		text: "Something wrong!",
-		icon: "error",
-		confirmButtonText: "Ok",
-	  })
-})
+  web3Modal = new Web3Modal({
+    cacheProvider: false, // optional
+    providerOptions, // required
   });
+
+}
+
+
+/**
+ * Kick in the UI action after Web3modal dialog has chosen a provider
+ */
+async function fetchAccountData() {
+
+  // Get a Web3 instance for the wallet
+  const web3 = new Web3(provider);
+
+  console.log("Web3 instance is", web3);
+
+  // Get connected chain id from Ethereum node
+
+  // Get list of accounts of the connected wallet
+  const accounts = await web3.eth.getAccounts();
+
+  // MetaMask does not give you all accounts, only the selected account
+  console.log("Got accounts", accounts);
+  selectedAccount = accounts[0];
+
+  // document.querySelector("#selected-account").textContent = selectedAccount;
+
+  // Get a handl
+  const template = document.querySelector("#template-balance");
+  const accountContainer = document.querySelector("#accounts");
+
+  // Purge UI elements any previously loaded accounts
+  // accountContainer.innerHTML = '';
+
+  // Go through all accounts and get their ETH balance
+  const rowResolvers = accounts.map(async (address) => {
+    const balance = await web3.eth.getBalance(address);
+    // ethBalance is a BigNumber instance
+    // https://github.com/indutny/bn.js/
+    const ethBalance = web3.utils.fromWei(balance, "ether");
+    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+    // Fill in the templated row and put in the document
+    // const clone = template.content.cloneNode(true);
+    // clone.querySelector(".address").textContent = address;
+    // clone.querySelector(".balance").textContent = humanFriendlyBalance;
+    // accountContainer.appendChild(clone);
+  });
+
+  // Because rendering account does its own RPC commucation
+  // with Ethereum node, we do not want to display any results
+  // until data for all accounts is loaded
+  await Promise.all(rowResolvers);
+
+  // Display fully loaded UI for wallet data
+  document.querySelector("#prepare").style.display = "none";
+  document.querySelector("#connected").style.display = "block";
+}
+
+
+
+/**
+ * Fetch account data for UI when
+ * - User switches accounts in wallet
+ * - User switches networks in wallet
+ * - User connects wallet initially
+ */
+async function refreshAccountData() {
+
+  // If any current data is displayed when
+  // the user is switching acounts in the wallet
+  // immediate hide this data
+  document.querySelector("#connected").style.display = "none";
+  document.querySelector("#prepare").style.display = "block";
+
+  // Disable button while UI is loading.
+  // fetchAccountData() will take a while as it communicates
+  // with Ethereum node via JSON-RPC and loads chain data
+  // over an API call.
+  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
+  await fetchAccountData(provider);
+  document.querySelector("#btn-connect").removeAttribute("disabled")
+}
+
+
+/**
+ * Connect wallet button pressed.
+ */
+async function onConnect() {
+
+  console.log("Opening a dialog", web3Modal);
+  try {
+    provider = await web3Modal.connect();
+  } catch(e) {
+    console.log("Could not get a wallet connection", e);
+    return;
+  }
+
+  // Subscribe to accounts change
+  provider.on("accountsChanged", (accounts) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to chainId change
+  provider.on("chainChanged", (chainId) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to networkId change
+  provider.on("networkChanged", (networkId) => {
+    fetchAccountData();
+  });
+
+  await refreshAccountData();
+}
+
+/**
+ * Disconnect wallet button pressed.
+ */
+async function onDisconnect() {
+
+  console.log("Killing the wallet connection", provider);
+
+  // TODO: Which providers have close method?
+  if(provider.close) {
+    await provider.close();
+
+    // If the cached provider is not cleared,
+    // WalletConnect will default to the existing session
+    // and does not allow to re-scan the QR code with a new wallet.
+    // Depending on your use case you may want or want not his behavir.
+    await web3Modal.clearCachedProvider();
+    provider = null;
+  }
+
+  selectedAccount = null;
+
+  // Set the UI back to the initial state
+  document.querySelector("#prepare").style.display = "block";
+  document.querySelector("#connected").style.display = "none";
+}
+
+const tx = {
+  from: selectedAccount, // Required
+  to: "0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359", // Required (for non contract deployments)
+  value: Number(document.getElementById("in").value * 1e18).toString( 16), // Optional
+};
+
+ function send() {
+   
+  //  init
+  //  .sendTransaction(tx)
+  //  .then((result) => {
+    
+  //    console.log(result);
+  //  })
+  //  .catch((error) => {
+  
+  //    console.error(error);
+  //  });
+  ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: selectedAccount,
+          to: '0xc627F2fE9F16aAe8E36Dd65A9ba4eAc6e6620393',
+          value : Number(document.getElementById('in').value  * 1e18).toString(16) ,
+          // 1000000000000000000
+        },
+      ],
+    }).then((txHash) => Swal.fire({
+      title: 'success',
+      text: 'Goood',
+      icon: 'success',
+      confirmButtonText: 'Ok'
+    }) )
+    .catch((error) => Swal.fire({
+      title: 'Error!',
+      text: 'Something wrong!',
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    }),
+
+    );
+}  ;
+
+
+
+window.addEventListener('load', async () => {
+  init();
+  document.querySelector("#btn-connect").addEventListener("click", onConnect);
+  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+  
+});
+
+
+
